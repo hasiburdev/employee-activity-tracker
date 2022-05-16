9f2fd4f4e6ccaf90631a97605515ab04d0541359
Hasibur Rahman <hasiburdev@gmail.com>
2022-05-16 21:34:26 +0600

    Add demo Data

---
+++ server/data/activityData.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    name: "Ecommerce",
+    duration: "2hr",
+    details: "Login Registration Page Done",
+  },
+  {
+    name: "Ecommerce",
+    duration: "2hr",
+    details: "Home Registration Page Done",
+  },
+  {
+    name: "Chat Application",
+    duration: "2hr",
+    details: "Login Page Done",
+  },
+  {
+    name: "Chat Application",
+    duration: "2hr",
+    details: "Registration Page Done",
+  },
+];
+
+export default data;
---
+++ server/data/employeeData.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    name: "Haibur Rahman",
+    designation: "Junior Developer",
+    officeTime: "N/A",
+    offday: "Everyday",
+    phone: "01010101010",
+  },
+  {
+    name: "Hasibur Rahman",
+    designation: "Junior Developer",
+    officeTime: "N/A",
+    offday: "Everyday",
+    phone: "01010101010",
+  },
+  {
+    name: "Hasibur Rahman",
+    designation: "Junior Developer",
+    offday: "Everyday",
+    phone: "01010101010",
+    officeTime: "11:00AM-8:00PM",
+  },
+];
+export default data;
